bb7cc3031dabae082cda34b29805f62a4b14ca9f
Wesley Ramalho <wesley_raa@hotmail.com>
2019-12-07 17:02:39 -0300

    lendo variáveis para gerar senha

--- 3/solution.py
+++ 3/solution.py
@@ -1,48 +1,54 @@
 # -*- coding: utf-8 -*-
 import io
 import sys
+import numbers
 
 tiposValidosDeSenha = 'abcde'
 
-tiposValidosDeSenha = ["a", "b", "c", "d", "e"]
+tiposValidosDeSenha = ["a", "b", "c", "d", "e", "A", "B", "C", "D", "E"]
+
+TipoGlobal = 'kk'
+TamanhoGlobal = 0
+
 
 def ValidarTipoSenha(senhaDigitada):
-  if tiposValidosDeSenha.count(senhaDigitada):
-    return True
-  return False
-    # LerTipoSenha()
+  if tiposValidosDeSenha.count(senhaDigitada): # se o tipo de senha for uma das opções disponíveis, retorna verdadeiro
+    return True 
+  return False # se não, retorna falso
 
 def ValidarTamanhoSenha(tamanhoDigitado):
-  # if(senhaDigitada not in tiposValidosDeSenha)
-    # print("Por favor, escolha um tipo de senha válido.")
-    # LerTipoSenha()
+  # if isinstance(tamanhoDigitado, int): # se o tamanho for uma variável do tipo inteiro, retorna verdadeiro
+  return True
+  return False
 
-
-# def LerTamanhoSenha():
-#     Tipo = sys.stdin.read(1) # Lê somente um caractere
 
 def LerTipoSenha():
-    print("Por favor, escolha um tipo de senha:/n")
-    print("a. Numérica – conterá apenas algarismos/n")
-    print("b. Alfabética – conterá apenas letras maiúsculas e minúsculas/n")
-    print("c. Alfanumérica 1 – conterá letras maiúsculas e algarismos/n")
-    print("d. Alfanumérica 2 – conterá letras maiúsculas, minúsculas e algarismos/n")
+    print("Por favor, escolha um tipo de senha:")
+    print("a. Numérica – conterá apenas algarismos")
+    print("b. Alfabética – conterá apenas letras maiúsculas e minúsculas")
+    print("c. Alfanumérica 1 – conterá letras maiúsculas e algarismos")
+    print("d. Alfanumérica 2 – conterá letras maiúsculas, minúsculas e algarismos")
     print("e. Geral – conterá letras maiúsculas, minúsculas, algarismos e os caracteres ASCII [33, 46] e [58, 64]")
-  # Tipo = sys.stdin.read(1) # Lê somente um caractere
-  # if ValidarTipoSenha(Tipo): 
-  #   print("Boa champs.")
-  # else:
-  #   print("Fez merda")
-  # print(Tipo)
-
+    Tipo = sys.stdin.read(1) # Lê somente um caractere
+    if ValidarTipoSenha(Tipo): 
+      TipoGlobal = Tipo
+      TamanhoGlobal = int (input ("Digite o tamanho da senha (número inteiro): "))
+      GeraSenha(TipoGlobal, TamanhoGlobal)
+    else:
+      print("Por favor, digite um tipo válido de senha.")
+      exit() # encerra o programa caso seja um tipo inválido
 
 def IniciarPrograma():
-  print("Iniciando Programa...")
-  # LerTipoSenha()
-
+  LerTipoSenha()
 
 
 def GeraSenha(Tipo, Tam):
+  print("Gerando senha...")
+  print("Tipo:")
+  print(Tipo)
+  print("Tamanho:")
+  print(Tam)
+  senha = 'ksdfjla'
   return senha
 
 IniciarPrograma()
