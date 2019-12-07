19493234d59a03b69c9f98d73b7824f9f038ecef
Wesley Ramalho <wesley_raa@hotmail.com>
2019-12-07 17:47:32 -0300

    gerando valores aleatórios com tamanho especifico

--- Exemplos/test.py
+++ Exemplos/test.py
@@ -4,6 +4,18 @@
 # seed(1)
 # generate some integers
 # values = randint(0, 10, 20)
-import random
-values = random.randrange(8923483)  
-print(values)
+# import random
+# values = random.randrange(8923483)  
+# print(values)
+
+
+from random import randint
+
+def random_with_N_digits(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return randint(range_start, range_end)
+
+print(random_with_N_digits(2))
+print(random_with_N_digits(3))
+print(random_with_N_digits(4))
