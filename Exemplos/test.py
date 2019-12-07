2e2622609079ee5463c7a5a523cd2387e32ba56f
Wesley Ramalho <wesley_raa@hotmail.com>
2019-12-07 17:44:43 -0300

    test to generate random number

---
+++ Exemplos/test.py
@@ -0,0 +1,9 @@
+# from numpy.random import seed
+# from numpy.random import randint
+# seed random number generator
+# seed(1)
+# generate some integers
+# values = randint(0, 10, 20)
+import random
+values = random.randrange(8923483)  
+print(values)
